633a44fe40270cf80070f2aa37ef3bd54d12131d
PotharajuKalyani-218 <23951a0473@iare.ac.in>
2025-07-26 19:00:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Arithmetic Operators/arithmetic-operators.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] computeOperations(int x, int y) {
+        int arr[]=new int[5];
+        arr[0]=(x+y);
+        arr[1]=(x-y);
+        arr[2]=(x*y);
+        arr[3]=(x/y);
+        arr[4]=(x%y);
+    return arr;
+    }
+}
