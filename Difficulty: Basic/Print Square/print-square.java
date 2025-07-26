bc6defe7984f856992ebefd04f6802f9d1cfb08f
PotharajuKalyani-218 <23951a0473@iare.ac.in>
2025-07-26 18:36:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Square/print-square.java
@@ -0,0 +1,18 @@
+// User function Template for Java
+class Solution {
+    public static void square(int s) {
+        
+        for(int i=1;i<=s;i++)
+        {
+            for(int j=1;j<=s;j++)
+            {
+                if(i==1 || i==s ||j==1||j==s)
+                System.out.print("* ");
+                else
+                System.out.print("  ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
