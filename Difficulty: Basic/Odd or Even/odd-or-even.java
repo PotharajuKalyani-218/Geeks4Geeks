d4e1c0ef372b415897a614070dd73a3ebd2d5b83
PotharajuKalyani-218 <23951a0473@iare.ac.in>
2026-02-25 15:00:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Odd or Even/odd-or-even.java
+++ Difficulty: Basic/Odd or Even/odd-or-even.java
@@ -1,6 +1,11 @@
 class Solution {
-    static boolean isEven(int n) {
-        // code here
-        return (n&1)==0;
+            public static boolean isEven(int n)
+    {
+        // taking bitwise and of n with 1 
+        if ((n & 1) == 0)
+            return true;
+        else
+            return false;
     }
-}
+
+    }
